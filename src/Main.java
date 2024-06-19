7c42919111f08f374d83182c47147f93438ecbbd
BrokenForks <whatley_gabriel@students.columbusstate.edu>
2024-06-19 10:11:02 -0400

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.String;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[] list1 = {"gabe", "daniel", "justin", "patrick"};
+        String[] list2 = {"mom", "dad", "brother", "sister"};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(Arrays.hashCode(list1));
+        System.out.println(Arrays.hashCode(list2));
     }
 }
